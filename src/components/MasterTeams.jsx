6851a77f571be2c0c25b0bdb501d5354a13d39b9
Massimo Pizzicannella <maxpizzi2605@gmail.com>
2025-01-06 21:01:18 +0100

    commit

--- src/components/MasterTeams.jsx
+++ src/components/MasterTeams.jsx
@@ -1,5 +1,22 @@
+import { Form, FormSelect } from 'react-bootstrap'
+
 const MasterTeams = () => {
-  return
+  return (
+    <>
+      <Form.Select style={{ borderRadius: '20px' }}>
+        <option value="">HC-NZ RU/SA/EM</option>
+        <option value="">HC-NZ DI/PE/PL</option>
+        <option value="">HC-NZ HG/SS</option>
+        <option value="">HC-NZ B/W</option>
+        <option value="">HC-NZ B2/W2</option>
+        <option value="">HC-NZ X/Y</option>
+        <option value="">HC-NZ OR/AS</option>
+        <option value="">HC-NZ SU/MO</option>
+        <option value="">HC-NZ US/UM</option>
+        <option value="">HC-NZ SWO/SH</option>
+      </Form.Select>
+    </>
+  )
 }
 
 export default MasterTeams
