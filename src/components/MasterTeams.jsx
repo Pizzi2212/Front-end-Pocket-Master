150d898626a23c4f7f3c60709abeabe0bd6efdd1
Massimo Pizzicannella <maxpizzi2605@gmail.com>
2025-03-07 18:19:33 +0100

    commit

--- src/components/MasterTeams.jsx
+++ src/components/MasterTeams.jsx
@@ -3,249 +3,116 @@ import { Container, Row, Col, Form } from 'react-bootstrap'
 import PokemonCard from './PokemonCard'
 import { useLocation } from 'react-router-dom'
 
-const MasterTeams = ({ data, userId }) => {
-  const [teams, setTeams] = useState({
-    'HC-NZ RU/SA/EM': [],
-    'HC-NZ DI/PE/PL': [],
-    'HC-NZ FIRE/LEAF': [],
-    'HC-NZ HG/SS': [],
-    'HC-NZ HG/SS KANTO': [],
-    'HC-NZ BW': [],
-    'HC-NZ BW2': [],
-    'HC-NZ X/Y': [],
-    'HC-NZ OR/AS': [],
-    'HC-NZ SUN/MOON': [],
-    'HC-NZ SWO/SH': [],
-    'Monotype Psychic DI/PE/PL': [],
-    'Monotype Normal DI/PE/PL': [],
-    'Monotype Bug DI/PE/PL': [],
-    'Monotype Fire BW': [],
-    'Monotype Water BW': [],
-    'Monotype Normal BW': [],
-    'Monotype Fairy X/Y': [],
-    'Monotype Flying X/Y': [],
-    'Monotype Grass X/Y': [],
-  })
-  const [selectedOption, setSelectedOption] = useState('HC-NZ RU/SA/EM')
+const MasterTeams = () => {
+  const [teams, setTeams] = useState([])
+  const [selectedTeam, setSelectedTeam] = useState('')
+  const [pokemonList, setPokemonList] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const location = useLocation()
 
-  const teamConfigurations = {
-    'HC-NZ RU/SA/EM': [
-      'swampert',
-      'breloom',
-      'vigoroth',
-      'hariyama',
-      'manectric',
-      'armaldo',
-    ],
-    'HC-NZ FIRE/LEAF': [
-      'blastoise',
-      'snorlax',
-      'jolteon',
-      'nidoking',
-      'exeggutor',
-      'arcanine',
-    ],
+  const fetchTeams = async () => {
+    try {
+      const bearer =
+        'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdHJpbmciLCJyb2xlcyI6WyJST0xFX0FETUlOIl0sImlhdCI6MTczOTg5MDI5MywiZXhwIjoxNzM5OTc2NjkzfQ.Citpq4uqNjuNbtQBgLcQh-CbSzttjHOlLrVdvsrQO-k' // 🔹 Prende il token JWT dal localStorage
 
-    'HC-NZ DI/PE/PL': [
-      'empoleon',
-      'luxray',
-      'gengar',
-      'bronzong',
-      'honchkrow',
-      'machamp',
-    ],
-    'HC-NZ HG/SS': [
-      'feraligatr',
-      'heracross',
-      'steelix',
-      'espeon',
-      'miltank',
-      'ampharos',
-    ],
-    'HC-NZ HG/SS KANTO': [
-      'venusaur',
-      'heracross',
-      'steelix',
-      'espeon',
-      'vaporeon',
-      'ampharos',
-    ],
-    'HC-NZ BW': [
-      'serperior',
-      'stoutland',
-      'scolipede',
-      'seismitoad',
-      'mandibuzz',
-      'chandelure',
-    ],
-    'HC-NZ BW2': [
-      'samurott',
-      'volcarona',
-      'lucario',
-      'gothitelle',
-      'zoroark',
-      'amoonguss',
-    ],
-    'HC-NZ X/Y': [
-      'greninja',
-      'charizard',
-      'gogoat',
-      'lucario',
-      'sylveon',
-      'ampharos',
-    ],
-    'HC-NZ OR/AS': [
-      'swampert',
-      'gardevoir',
-      'flygon',
-      'sableye',
-      'crobat',
-      'camerupt',
-    ],
-    'HC-NZ SUN/MOON': [
-      'decidueye',
-      'salamence',
-      'espeon',
-      'golisopod',
-      'toucannon',
-      'mudsdale',
-    ],
-    'HC-NZ SWO/SH': [
-      'rillaboom',
-      'grimmsnarl',
-      'corviknight',
-      'dragapult',
-      'toxtricity-amped',
-      'lapras',
-    ],
-    'Monotype Psychic DI/PE/PL': [
-      'medicham',
-      'alakazam',
-      'girafarig',
-      'bronzong',
-      'gallade',
-      'gardevoir',
-    ],
-    'Monotype Normal DI/PE/PL': [
-      'staraptor',
-      'bibarel',
-      'lopunny',
-      'blissey',
-      'porygon-z',
-      'snorlax',
-    ],
-    'Monotype Bug DI/PE/PL': [
-      'kricketune',
-      'wormadam-plant',
-      'mothim',
-      'vespiquen',
-      'dustox',
-      'yanmega',
-    ],
-    'Monotype Fire BW': [
-      'emboar',
-      'simisear',
-      'chandelure ',
-      'darmanitan-standard',
-      'heatmor',
-    ],
-    'Monotype Water BW': [
-      'samurott',
-      'seismitoad',
-      'basculin-red-striped',
-      'carracosta',
-      'swanna',
-      'jellicent',
-    ],
-    'Monotype Normal BW': [
-      'watchog',
-      'stoutland',
-      'cinccino',
-      'audino',
-      'bouffalant',
-      'braviary',
-    ],
-    'Monotype Fairy X/Y': [
-      'florges',
-      'sylveon',
-      'azumarill',
-      'dedenne',
-      'aromatisse',
-      'granbull',
-    ],
-    'Monotype Flying X/Y': [
-      'talonflame',
-      'vivillon',
-      'hawlucha',
-      'noivern',
-      'crobat',
-      'gyarados',
-    ],
-    'Monotype Grass X/Y': [
-      'chesnaught',
-      'venusaur',
-      'gogoat',
-      'trevenant',
-      'whimsicott',
-      'amoonguss',
-    ],
+      const response = await fetch('http://localhost:8080/teams', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${bearer}`,
+        },
+      })
+
+      if (response.status === 401) {
+        throw new Error('Not authorized. Make sure you are logged in.')
+      }
+
+      const data = await response.json()
+      console.log('Team ricevuti:', data)
+      setTeams(data)
+
+      if (data.length > 0) {
+        setSelectedTeam(data[0].id)
+      }
+    } catch (error) {
+      console.error('Error fetching teams:', error)
+    }
   }
 
-  const fetchPokemonForTeam = async (teamName) => {
+  const fetchPokemonForTeam = async (teamId) => {
+    if (!teamId) return
     setIsLoading(true)
-    const pokemonNames = teamConfigurations[teamName] || []
-    const promises = pokemonNames.map(async (name) => {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-      const json = await response.json()
-      return parsePokeAPIResponse(json)
-    })
-
     try {
-      const fetchedPokemons = await Promise.all(promises)
-      setTeams((prev) => ({ ...prev, [teamName]: fetchedPokemons }))
+      const bearer =
+        'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdHJpbmciLCJyb2xlcyI6WyJST0xFX0FETUlOIl0sImlhdCI6MTczOTg5MDI5MywiZXhwIjoxNzM5OTc2NjkzfQ.Citpq4uqNjuNbtQBgLcQh-CbSzttjHOlLrVdvsrQO-k'
+
+      const response = await fetch(`http://localhost:8080/teams/${teamId}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${bearer}`,
+        },
+      })
+
+      if (response.status === 401) {
+        throw new Error('Not authorized. Make sure you are logged in.')
+      }
+
+      const data = await response.json()
+      console.log('Dati del team ricevuti:', data)
+
+      const parsedPokemons = data.pokemons.map(parsePokeAPIResponse)
+
+      setPokemonList(parsedPokemons)
     } catch (error) {
-      console.error('Error fetching Pokémon:', error)
+      console.error('Error fetching team:', error)
     } finally {
       setIsLoading(false)
     }
   }
 
+  useEffect(() => {
+    fetchTeams()
+  }, [])
+
+  useEffect(() => {
+    if (selectedTeam) {
+      fetchPokemonForTeam(selectedTeam)
+    }
+  }, [selectedTeam])
+
   const parsePokeAPIResponse = (json) => ({
     name: json.name,
     weight: json.weight,
     height: json.height,
-    spriteStat: json.sprites.other?.showdown?.front_default,
-    sprite: json.sprites.other?.['official-artwork']?.front_default,
-    spriteShiny: json.sprites.other?.['official-artwork']?.front_shiny,
-    abilities: json.abilities.map((e) => e.ability.name),
-    moves: json.moves.map((e) => e.move.name),
-    stats: json.stats.map((e) => ({
-      name: e.stat.name,
-      value: e.base_stat,
-    })),
-    types: json.types.map((e) => e.type.name),
+    spriteStat: json.sprites?.other?.showdown?.front_default || '',
+    sprite: json.sprites?.other?.['official-artwork']?.front_default || '',
+    spriteShiny: json.sprites?.other?.['official-artwork']?.front_shiny || '',
+    abilities: json.abilities ? json.abilities.map((e) => e.ability.name) : [],
+    moves: json.moves ? json.moves.map((e) => e.move.name) : [],
+    stats: json.stats
+      ? json.stats.map((e) => ({
+          name: e.stat.name,
+          value: e.base_stat,
+        }))
+      : [],
+    types: json.types ? json.types.map((e) => e.type.name) : ['unknown'],
   })
 
-  useEffect(() => {
-    fetchPokemonForTeam(selectedOption)
-  }, [selectedOption])
-
   const handleOptionChange = (e) => {
-    setSelectedOption(e.target.value)
+    setSelectedTeam(e.target.value)
   }
 
   return (
     <Container>
       <Form.Select
-        value={selectedOption}
+        value={selectedTeam}
         onChange={handleOptionChange}
         className="mb-3 mt-3"
       >
-        {Object.keys(teamConfigurations).map((option) => (
-          <option key={option} value={option}>
-            {option}
+        {teams.map((team) => (
+          <option key={team.id} value={team.id}>
+            {team.name}
           </option>
         ))}
       </Form.Select>
@@ -259,7 +126,7 @@ const MasterTeams = ({ data, userId }) => {
         </div>
       ) : (
         <Row>
-          {teams[selectedOption].map((pokemon, index) => (
+          {pokemonList.map((pokemon, index) => (
             <Col key={index} xs={12} md={6} lg={4}>
               <PokemonCard data={pokemon} useLocation={location.pathname} />
             </Col>
